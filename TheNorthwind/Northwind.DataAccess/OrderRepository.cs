3ec392a3ad59572d0474353f8b7abc3690be3c5d
Emre Burak GÜL <emreburakgul@gmail.com>
2022-06-15 01:48:03 +0300

    CRUD operations with Dapper

---
+++ TheNorthwind/Northwind.DataAccess/OrderRepository.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Northwind.Entities;
+
+namespace Northwind.DataAccess
+{
+    public class OrderRepository
+    {
+        public IEnumerable<Order> GetAllOrders()
+        {
+            var order = Enumerable.Empty<Order>();
+            using (var connection = DBConnectionFactory.Create())
+            {
+                return connection.Query<Order>("Select * from Orders");
+            }
+        }
+
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            var customer = Enumerable.Empty<Customer>();
+
+            using (var connection = DBConnectionFactory.Create())
+            {
+                return connection.Query<Customer>("select * from Customers");
+            }
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            var employee = Enumerable.Empty<Employee>();
+
+            using (var connection = DBConnectionFactory.Create())
+            {
+                return connection.Query<Employee>("select * from Employees");
+            }
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            var product = Enumerable.Empty<Product>();
+
+            using (var connection = DBConnectionFactory.Create())
+            {
+                return connection.Query<Product>("select * from Products");
+            }
+        }
+
+        public IEnumerable<Shipper> GetAllShippers()
+        {
+            var shipper = Enumerable.Empty<Shipper>();
+
+            using (var connection = DBConnectionFactory.Create())
+            {
+                return connection.Query<Shipper>("select * from Shippers");
+            }
+        }
+
+        public Order Find(int orderId)
+        {
+            using (var connection = DBConnectionFactory.Create())
+            {
+                const string orderSqlText = "select * from [Orders] where OrderID = @orderId";
+                const string detailSqlText = @"
+select
+    od.*,
+    NULL as Seperator,
+    p.*
+from 
+[Order Details]  od
+inner join Products p on od.ProductID = p.ProductID
+where OrderID = @orderId";
+
+                var parameters = new { orderId = orderId };
+                var order = connection.QuerySingleOrDefault<Order>(
+                    orderSqlText,
+                    parameters);
+
+                if (order != null)
+                {
+                    var orderDetails = connection.Query<OrderDetail, Product, OrderDetail>(
+                        detailSqlText,
+                        (detail, product) =>
+                        {
+                            detail.Product = product;
+                            return detail;
+                        },
+                        // MapQueryResult,
+                        parameters,
+                        splitOn: "Seperator");
+
+                    foreach (var detail in orderDetails)
+                    {
+                        order.OrderDetails.Add(detail);
+                    }
+                }
+
+                return order;
+            }
+        }
+
+        public void Add(Order order)
+        {
+            using (var connection = DBConnectionFactory.Create())
+            {
+                const string orderInsertSql = @"
+insert into Orders (CustomerID,
+EmployeeID,
+OrderDate,
+RequiredDate,
+ShippedDate,
+ShipVia,
+ShipName,
+Freight,
+ShipAddress,
+ShipCity,
+ShipRegion,
+ShipPostalCode,
+ShipCountry)
+values (@CustomerID,
+@EmployeeID,
+@OrderDate,
+@RequiredDate,
+@ShippedDate,
+@ShipVia,
+@ShipName,
+@Freight,
+@ShipAddress,
+@ShipCity,
+@ShipRegion,
+@ShipPostalCode,
+@ShipCountry)
+
+select @@IDENTITY;";
+
+                // Execute and Get OrderID!
+
+                order.OrderID = connection.ExecuteScalar<int>(orderInsertSql, order);
+
+
+                const string orderDetailInsertSql = @"
+insert into [Order Details]
+(OrderID,
+ProductID,
+UnitPrice,
+Quantity,
+Discount)
+values (@OrderID, @ProductID, @UnitPrice, @Quantity, @Discount)";
+
+                foreach (var orderDetail in order.OrderDetails)
+                {
+                    orderDetail.OrderID = order.OrderID;
+
+                    connection.Execute(orderDetailInsertSql, orderDetail);
+                }
+            }
+        }
+
+        public void Remove(Order orderDetails)
+        {
+            throw new NotImplementedException();
+        }
+
+        private OrderDetail MapQueryResult(OrderDetail orderDetail, Product product)
+        {
+            orderDetail.Product = product;
+            return orderDetail;
+        }
+    }
+}
---
+++ TheNorthwind/Northwind.Presentation/OrderGridForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Northwind.Presentation
+{
+    partial class OrderGridForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridOrder = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuItemUpdateOrder = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemDeleteOrder = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridOrder)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridOrder
+            // 
+            this.dataGridOrder.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridOrder.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridOrder.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridOrder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridOrder.Location = new System.Drawing.Point(0, 0);
+            this.dataGridOrder.MultiSelect = false;
+            this.dataGridOrder.Name = "dataGridOrder";
+            this.dataGridOrder.ReadOnly = true;
+            this.dataGridOrder.RowHeadersWidth = 51;
+            this.dataGridOrder.RowTemplate.ContextMenuStrip = this.contextMenuStrip1;
+            this.dataGridOrder.RowTemplate.Height = 25;
+            this.dataGridOrder.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridOrder.Size = new System.Drawing.Size(800, 450);
+            this.dataGridOrder.TabIndex = 0;
+            this.dataGridOrder.CellMouseDown += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridOrder_CellMouseDown);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuItemUpdateOrder,
+            this.menuItemDeleteOrder});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 70);
+            // 
+            // menuItemUpdateOrder
+            // 
+            this.menuItemUpdateOrder.Name = "menuItemUpdateOrder";
+            this.menuItemUpdateOrder.Size = new System.Drawing.Size(180, 22);
+            this.menuItemUpdateOrder.Text = "Update";
+            this.menuItemUpdateOrder.Click += new System.EventHandler(this.menuItemUpdateOrder_Click);
+            // 
+            // menuItemDeleteOrder
+            // 
+            this.menuItemDeleteOrder.Name = "menuItemDeleteOrder";
+            this.menuItemDeleteOrder.Size = new System.Drawing.Size(180, 22);
+            this.menuItemDeleteOrder.Text = "Delete";
+            this.menuItemDeleteOrder.Click += new System.EventHandler(this.menuItemDeleteOrder_Click);
+            // 
+            // OrderGridForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridOrder);
+            this.Name = "OrderGridForm";
+            this.Text = "OrderGridForm";
+            this.Load += new System.EventHandler(this.OrderGridForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridOrder)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridOrder;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuItemUpdateOrder;
+        private System.Windows.Forms.ToolStripMenuItem menuItemDeleteOrder;
+    }
+}
